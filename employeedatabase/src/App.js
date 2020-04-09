ebce134c7739ad52ea3aab3aaad1fb8b0593ffc4
Your Name <you@example.com>
2020-04-09 09:37:42 -0400

    Table added to display with NavBar.

--- employeedatabase/src/App.js
+++ employeedatabase/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from './components/Header/Header.js'
+import Table from './components/Table/Table.js'
 
 const App = () => {
   return (
     <div className="App">
       <Header />
+      <Table />
     </div>
   );
 }
---
+++ employeedatabase/src/components/Table/Table.js
@@ -0,0 +1,30 @@
+import React from 'react';
+//import 
+
+const Table = () => {
+    return (
+      <table border="2px">
+          <thead>
+              <tr>
+                  <th>Image</th>
+                  <th>Name</th>
+                  <th>Phone</th>
+                  <th>Email</th>
+                  <th>DOB</th>
+              </tr>
+          </thead>
+          <tbody>
+              <tr>
+                  <td>I</td>
+                  <td>N</td>
+                  <td>88</td>
+                  <td>a@a</td>
+                  <td>DOB</td>
+              </tr>
+          </tbody>
+      </table>
+    )
+}
+
+export default Table;
+
